fd869941f00951fdaa07cc2fa03fbcfe1b561c77
kelvin <kelvinc1291@hotmail.com>
2023-08-13 00:48:55 -0300

    feat: implementa component de Dash

---
+++ src/components/Dash/index.tsx
@@ -0,0 +1,18 @@
+import { CardButton } from '../CardButton';
+import { Card } from '../Card';
+import './style.css';
+
+
+
+export const Dash = () => {
+  return (
+    <section className="dash">
+        <div className="container">
+          <Card tipo="Entradas" svg="trending-up.svg" color='var(--background-svg-up)' cardValue="1853,12" />
+          <Card tipo="Saida" svg="trending-down.svg" color='var(--background-svg-down)' cardValue="1853,12" />
+          <Card tipo="Entradas" svg="dolar.svg" cardValue="1853,12" />
+          <CardButton/>
+        </div>
+    </section>
+  );
+};

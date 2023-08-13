b5787924f1c927eac64912c1144170d985eabaf8
kelvin <kelvinc1291@hotmail.com>
2023-08-13 17:05:28 -0300

    feat: implementa componente Transation

--- src/components/Transations/index.tsx
+++ src/components/Transations/index.tsx
@@ -1,3 +1,4 @@
+import { Table } from '../Table';
 import './style.css';
 
 export const Transations = () => {
@@ -5,34 +6,7 @@ export const Transations = () => {
     <section className="transations">
       <div className="container">
         <div className="transation-table">
-          <h3 className="title-transations">Histórico de transações</h3>
-          <table className="table">
-            <thead className='pp'>
-              <tr>
-                <th>Nome</th>
-                <th>Data</th>
-                <th>Categoria</th>
-                <th>Valor</th>
-                <th>Tipo</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Venda de celular antigo</td>
-                <td>01/08/2023</td>
-                <td>Renda extra</td>
-                <td>R$ 1.853,12</td>
-                <td>+</td>
-              </tr>
-              <tr>
-                <td>Venda de celular antigo</td>
-                <td>01/08/2023</td>
-                <td>Renda extra</td>
-                <td>R$ 1.853,12</td>
-                <td>-</td>
-              </tr>
-            </tbody>
-          </table>
+          <Table />
         </div>
       </div>
     </section>

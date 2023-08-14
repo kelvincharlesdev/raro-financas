48daac5c3cb6f28c7ba70eb77571d285405708a8
kelvin <kelvinc1291@hotmail.com>
2023-08-14 00:03:29 -0300

    style: implementa estilos de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import './App.css';
 import { Dash } from './components/Dash';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
+import { Modal } from './components/Modal';
 import { Transations } from './components/Transations';
 
 function App() {
   return (
     <>
+      
       <Header />
+     
       <main>
+        <Modal />
         <Dash />
         <Transations />
+       
       </main>
       <Footer/>
+
+    
     </>
   );
 }

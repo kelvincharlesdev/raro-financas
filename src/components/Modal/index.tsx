eb2a184b5401a975ea0156f20688edcf16b11d11
kelvin <kelvinc1291@hotmail.com>
2023-08-16 13:48:13 -0300

    implementa funcionalidade Modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,12 +1,21 @@
+import { ITransaction } from '../../types';
 import { Form } from '../Form';
-
 import './style.css';
 
-export const Modal = () => {
+interface IModal {
+  mostrarModal: boolean;
+  callBack: ( transacaoInformacao : ITransaction) => void;
+  fecharModal: (mostrarModal: boolean) => void
+  
+}
+
+export const Modal = ({mostrarModal, callBack, fecharModal}: IModal) => {
+
+
   return (
-    <div className="modal">
+    <div className={`modal ${mostrarModal && 'open'}`}>
       <div className="content-modal">
-        <Form />
+        <Form callBack={callBack} fecharModal={(mostrarModal: boolean) => fecharModal(mostrarModal)} />
       </div>
     </div>
   );

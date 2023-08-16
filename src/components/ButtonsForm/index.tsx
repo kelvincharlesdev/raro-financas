ab78a4c213a6c70d6dfc1040a52716eb705962a9
kelvin <kelvinc1291@hotmail.com>
2023-08-16 13:45:37 -0300

    implementa funcionalidade ButtonsForm

--- src/components/ButtonsForm/index.tsx
+++ src/components/ButtonsForm/index.tsx
@@ -5,11 +5,13 @@ interface IButtonForm {
     text: string;
     className: string; 
     type?: "button" | "submit";
+    
+    calllack?: () => void;
   }
   
-  export const ButtonForm = ({ className, imgSrc, text, type }: IButtonForm) => {
+  export const ButtonForm = ({ className, imgSrc, text, type, calllack }: IButtonForm) => {
     return (
-      <button className={className} type={type}>
+      <button className={className} type={type} onClick={calllack}>
         <img src={imgSrc} alt={text}/>
         {text}
       </button>

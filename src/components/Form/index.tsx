72703d49b5fcc6968f427a406bd7b5129397fc9d
kelvin <kelvinc1291@hotmail.com>
2023-08-16 13:47:21 -0300

    implementa funcionalidade Form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,40 +1,71 @@
+
+import { ITransaction } from '../../types';
 import { ButtonForm } from '../ButtonsForm';
 import { Input } from '../Input';
 
 import './style.css';
 
-export const Form = () => {
+interface IForm {
+  callBack: ( transacaoInformacao : ITransaction) =>  void;
+  fecharModal: (mostratModal: boolean) => void;
+}
+
+export const Form = ({ callBack, fecharModal }: IForm) => {
+ 
+  const transacaoInformacao = (e : React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); 
+   
+    const inputValue = {
+      nome: (e.currentTarget.elements.namedItem('name') as HTMLInputElement).value,
+      data: (e.currentTarget.elements.namedItem('data') as HTMLInputElement).value,
+      categoria: (e.currentTarget.elements.namedItem('categoria') as HTMLInputElement).value,
+      valor: (e.currentTarget.elements.namedItem('valor') as HTMLInputElement).value,
+      tipo: (e.currentTarget.elements.namedItem('tipo') as HTMLInputElement).value,
+    };
+
+    callBack(inputValue) 
+    e.currentTarget.reset()
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={transacaoInformacao}>
       <h2 className="title-form">Nova transação</h2>
 
       <div className="content-form">
         <Input
+          id="name"
           text="Nome"
           type="text"
           name="name"
           placeholder="Digite o nome da sua transação"
+        
         />
 
         <Input
+          id="data"
           text="Data"
           type="date"
           name="data"
           placeholder="Digite o nome da sua transação"
+         
         />
 
         <Input
+          id="categoria"
           text="Categoria"
           type="text"
           name="categoria"
           placeholder="Digite a categoria da transação"
+         
         />
 
         <Input
-          text="Valor"
+          id="valor"
+          text="valor"
           type="number"
           name="valor"
           placeholder="Digite o valor da sua transação"
+       
         />
 
         <div className="input">
@@ -45,7 +76,7 @@ export const Form = () => {
               type="radio"
               id="entrada"
               name="tipo"
-              value="entrada"
+              value="+"
               required
             />
             <label htmlFor="entrada">
@@ -53,7 +84,7 @@ export const Form = () => {
               Entrada
             </label>
 
-            <input type="radio" id="saida" name="tipo" value="saida" required />
+            <input type="radio" id="saida" name="tipo" value="-" required />
             <label htmlFor="saida">
               <img
                 src="src/assets/trending-down.svg"
@@ -70,6 +101,7 @@ export const Form = () => {
           className="btn-modal cancelar"
           imgSrc="src/assets/x.svg"
           text="Cancelar"
+          calllack={() => fecharModal(false)}
         />
         <ButtonForm
           className="btn-modal adicionar"

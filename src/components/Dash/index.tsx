f175c26f2fe16c99dd76771f91f0d8f18fc89edb
Kelvin Charles <kelvinc1291@hotmail.com>
2023-10-24 10:36:08 -0300

    fix: Implementa correção em imagem tranding-up

--- src/components/Dash/index.tsx
+++ src/components/Dash/index.tsx
@@ -1,9 +1,6 @@
 import { CardButton } from '../CardButton';
 import { Card } from '../Card';
 import './style.css';
-import '../../assets/dolar.svg'
-import '../../assets/trending-up.svg'
-import '../../assets/trending-down.svg'
 
 interface IDash {
   callback: () => void;
